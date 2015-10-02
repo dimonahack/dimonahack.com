97835f561dece2cfa5d2f66d91a6d840eb38b265
ET-CS <et.programming@gmail.com>
2015-10-02 18:32:44 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+    include "";
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="description" content="" />
+        <meta name="keywords" content="">
+        <meta name="author" content="Websiter.co.il - Nir Gofman">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <script src="js/jquery.js"></script>
+    </head>
+    <body>
+        <div class="topSection">
+            <div class="logoArea">
+                <h1>DIMON@HACK</h1>
+            </div>
+            <a class="eventDetailsLink" href="Details.php">פרטי האירוע</a>
+            <a class="registerLink" href="Details.php">הרשמה</a>
+        </div>
+        <div class="topSection">
+            <div class="logoArea">
+                <table class="timeTable">
+                    <tr class="topTr">
+                        <td>ימים</td>
+                        <td>שעות</td>
+                        <td>דקות</td>
+                        <td>שניות</td>
+                    </tr>
+                    <tr class="timeTr">
+                        <td style="border-left: 1px solid #CC3E00;">30</td>
+                        <td style="border-left: 1px solid #CC3E00;">26</td>
+                        <td style="border-left: 1px solid #CC3E00;">6</td>
+                        <td>130</td>
+                    </tr>
+                </table>
+            </div>
+            <div class="freeSpace">
+                <h2>#האקתוןדימונההראשון</h2>
+                <span>בואו תעשו הסטוריה והצטרפו להאקתון הראשון בדימונה! 19-20 בנובמבר.</span>
+            </div>
+        </div>
+        <h1 class="centerLogo">DIMON@HACK<span>דימונה היא לא משחשבתם.. עולם אחר.. שונה.. מתקדם..</span></h1>
+    </body>
+</html>
