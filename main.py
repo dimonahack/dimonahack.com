27103f69eff668ab41ad2b970ff2d07ce5a3122e
ET-CS <et.programming@gmail.com>
2015-10-09 18:17:39 +0300

    new 'signup' page & update dates

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return render_template('index.html')
 
+
 @app.route("/details")
 def details():
     return render_template('details.html')
+
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
 
 if __name__ == "__main__":
     app.run(port=8576)
